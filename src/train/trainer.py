9b13b23f3a16f1fc058d3f0a94bf2f7783dd369e
Tomas <horychtomas98@gmail.com>
2023-08-11 18:58:52 +0900

    edit trainer

--- src/train/trainer.py
+++ src/train/trainer.py
@@ -32,7 +32,6 @@ class Trainer():
         self.eval_steps = training_args['eval_steps']
         self.logging_steps = training_args['logging_steps']
 
-
         self.tokenizer = model.tokenizer
         self.device = model.device
         self.model = model.model
@@ -45,25 +44,30 @@ class Trainer():
         dl = self.dataset.train
         optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.lr)
         loss = torch.nn.CrossEntropyLoss()
-        loss_sum=0
+        loss_sum = 0
 
+        no_steps = self.epochs * (len(dl))/self.batch_size
         self.model.train()
-        for i, batch in tqdm(enumerate(dl)):
-            optimizer.zero_grad()
-            outputs = self.model(**batch)
-            loss = outputs.loss
-            loss_sum+=loss.item()
-            loss.backward()
-            optimizer.step()
-            
-            if (i+1) % self.eval_steps == 0:
-                f1,dev_loss = self.eval(split='eval')
-                wandb.log({'dev_loss':dev_loss,'dev_f1':f1})
-                self.model.train()
 
-            if (i+1)% self.logging_steps == 0:
-                wandb.log({'train_loss':loss_sum/self.logging_steps})
-                loss_sum=0
+        for step in tqdm(no_steps):
+            for i, batch in tqdm(enumerate(dl)):
+                optimizer.zero_grad()
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+
+                outputs = self.model(**batch)
+                loss = outputs.loss
+                loss_sum += loss.item()
+                loss.backward()
+                optimizer.step()
+
+                if (i+1) % self.eval_steps == 0:
+                    f1, dev_loss = self.eval(split='eval')
+                    wandb.log({'dev_loss': dev_loss, 'dev_f1': f1})
+                    self.model.train()
+
+                if (i+1) % self.logging_steps == 0:
+                    wandb.log({'train_loss': loss_sum/self.logging_steps})
+                    loss_sum = 0
 
         return self.model
 
@@ -71,8 +75,8 @@ class Trainer():
         metric1 = load_metric("f1")
 
         dl = self.dataset.test if split == 'test' else self.dataset.dev
-        
-        loss_sum=0
+
+        loss_sum = 0
         self.model.eval()
         for batch in tqdm(dl):
             batch = {k: v.to(self.device) for k, v in batch.items()}
@@ -80,9 +84,9 @@ class Trainer():
                 outputs = self.model(**batch)
 
             logits = outputs.logits
-            loss_sum+=outputs.loss.item()
+            loss_sum += outputs.loss.item()
             predictions = torch.argmax(logits, dim=-1)
             metric1.add_batch(predictions=predictions,
                               references=batch["labels"])
 
-        return metric1.compute(average='macro')['f1'],loss_sum/(self.batch_size*len(dl))
+        return metric1.compute(average='macro')['f1'], loss_sum/(self.batch_size*len(dl))
