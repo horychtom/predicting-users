b07647515777eabd041988242d2970a7a0962c89
Tomas <horychtomas98@gmail.com>
2023-08-17 19:57:56 +0900

    d

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -11,7 +11,7 @@ training_args = TrainingArguments(
     report_to='wandb',
     output_dir='./',
     overwrite_output_dir=True,
-    num_train_epochs=5,
+    num_train_epochs=10,
     per_device_train_batch_size=64,
     per_device_eval_batch_size=64,
     do_eval=True,
@@ -26,24 +26,6 @@ training_args = TrainingArguments(
 
 
 # regression on titles
-training_args.run_name = 'all_all_regression'
-model = HFmodel(checkpoint='xlm-roberta-base',num_classes=1)
-ds = DataSet('datasets/all_all.csv', model.tokenizer,
-             {'seo_title': 'text', 'y': 'labels'}, use_dataloaders=False)
-trainer = TrainerWrapper(training_args=training_args, dataset=ds, model=model,
-                         project_name="testing_data_modelling", run_name=training_args.run_name)
-trainer.train()
-wandb.finish()
-
-training_args.run_name = 'all_all_regression_on_merged'
-model = HFmodel(checkpoint='xlm-roberta-base',num_classes=1)
-ds = DataSet('datasets/all_all.csv', model.tokenizer,
-             {'merged': 'text', 'total_pageviews': 'labels'}, use_dataloaders=False)
-trainer = TrainerWrapper(training_args=training_args, dataset=ds, model=model,
-                         project_name="testing_data_modelling", run_name=training_args.run_name)
-trainer.train()
-wandb.finish()
-
 training_args.run_name = 'all_all_clf'
 model = HFmodel(checkpoint='xlm-roberta-base',num_classes=3)
 ds = DataSet('datasets/all_all.csv', model.tokenizer,
